a02f8b110f2f73130296de037408c45a79be2ba1
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-31 23:14:19 +0000

    in progress, added function to return a DOM tree that matches entries in HTML

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var $imgEntry = document.querySelector('#img-entry');
 var $formElement = document.querySelector('form');
 $formElement.addEventListener('submit', saveEntryHandle);
 
+var $entryView = document.querySelector('#entry-view-ul');
+
 function updateImgURLHandle(event) {
   $imgEntry.setAttribute('src', event.target.value);
 }
@@ -22,4 +24,33 @@ function saveEntryHandle(event) {
   data.nextEntryId++;
   $imgEntry.setAttribute('src', 'images/placeholder-image-square.jpg');
   $formElement.reset();
+}
+
+function displayEntries(entry) {
+  var $notesElement = document.createElement('p');
+  $notesElement.textContent = entry.notes;
+  var $titleElement = document.createElement('h2');
+  $titleElement.textContent = entry.title;
+  var $divEntryText = document.createElement('div');
+  $divEntryText.setAttribute('class', 'column-half');
+  var $entryImg = document.createElement('img');
+  $entryImg.setAttribute('src', entry.imgURL);
+  var $entryImgFrame = document.createElement('div');
+  $entryImgFrame.setAttribute('class', 'img-frame');
+  var $divEntryImg = document.createElement('div');
+  $divEntryImg.setAttribute('class', 'column-half');
+  var $listElement = document.createElement('li');
+  $listElement.setAttribute('class', 'row');
+  $listElement.appendChild($divEntryImg);
+  $listElement.appendChild($divEntryText);
+  $divEntryImg.appendChild($entryImgFrame);
+  $entryImgFrame.appendChild($entryImg);
+  $divEntryText.appendChild($titleElement);
+  $divEntryText.appendChild($notesElement);
+  return $listElement;
+}
+
+for (var i = 0; i < data.entries.length; i++) {
+  var $entry = displayEntries(data.entries[i]);
+  $entryView.appendChild($entry);
 }
